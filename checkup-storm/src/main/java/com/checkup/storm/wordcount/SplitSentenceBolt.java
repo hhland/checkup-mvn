4dd003b290882c38f8fff2475145d6f5c0860f19
hhlin <smartken0824@gmail.com>
2014-10-27 17:40:21 +0800

    sample

---
+++ checkup-storm/src/main/java/com/checkup/storm/wordcount/SplitSentenceBolt.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.checkup.storm.wordcount;
+
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+import java.util.Map;
+import java.util.stream.Collector;
+
+/**
+ *
+ * @author Administrator
+ */
+public class SplitSentenceBolt extends BaseRichBolt {
+
+    private OutputCollector collector;
+
+    public void prepare(Map config, TopologyContext context,
+            OutputCollector collector) {
+        this.collector = collector;
+    }
+
+    public void execute(Tuple tuple) {
+        String sentence = tuple.getStringByField("sentence");
+        String[] words = sentence.split(" ");
+        for (String word : words) {
+            this.collector.emit(new Values(word));
+        }
+    }
+
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        declarer.declare(new Fields("word"));
+    }
+
+}
